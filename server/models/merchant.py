de697c4d6f51c0703d8c642a719181dcaec729af
Jared Hanson <jred0011@gmail.com>
2017-07-13 10:44:02 -0700

    Primary key and indentation

--- server/models/merchant.py
+++ server/models/merchant.py
@@ -1,9 +1,9 @@
 from . import db
 
 class Merchant(db.Model):
-     __tablename__ = 'merchant'
+    __tablename__ = 'merchant'
 
-    id          = db.Column(db.String(1000))
-    name        = db.Column(db.String(50))
-    menu        = db.relationship('MenuItem', backref='merchant', lazy='dynamic')
-    reciepts    = db.relationship('Reciept', backref='merchant', lazy='dynamic')
+    id = db.Column(db.String(1000), primary_key=True)
+    name = db.Column(db.String(50))
+    menu = db.relationship('MenuItem', backref='merchant', lazy='dynamic')
+    reciepts = db.relationship('Reciept', backref='merchant', lazy='dynamic')
